e8416425c27386f96d19ace99595dbbc7ee2a4ae
Justin Hugon <5174443+jhugon@users.noreply.github.com>
2018-10-05 15:25:19 -0500

    starting to make script and sub file

---
+++ run_larsoft.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+date
+cd $TMPDIR
+pwd
+echo "dummy run larsoft"
+echo "args: "$@
+date
+echo "=================================="
+env
+echo "=================================="
+ls -lhtr
