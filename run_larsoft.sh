c9de5e0fde3ab62278bf709b2ca9cdb4cfa0dfbf
Justin Hugon <5174443+jhugon@users.noreply.github.com>
2018-10-11 16:13:40 -0500

    added larsoft_job.sub

--- run_larsoft.sh
+++ run_larsoft.sh
@@ -5,8 +5,10 @@ cd $TMPDIR
 pwd
 echo "dummy run larsoft"
 echo "args: "$@
-date
+command="lar -c $1 -n $2"
+echo "command: "$command
 echo "=================================="
 env
 echo "=================================="
 ls -lhtr
+date
